744fd1b04a566406c48bd3de5421ab284c3e94d6
Maxim Levandovskiy <90237902+oscuroplan@users.noreply.github.com>
2021-10-06 09:27:05 +0300

    Update autocheck3.js

--- autocheck3.js
+++ autocheck3.js
@@ -1,1 +1,29 @@
+                                                                                // ЗАДАЧА 1/41
+// Присвой переменной apartment объект описывающий квартиру со следующими характеристиками:
 
+// imgUrl - фотография, значение "https://via.placeholder.com/640x480";
+// descr - описание, значение "Spacious apartment in the city center";
+// rating - рейтинг, значение 4;
+// price - цена, значение 2153;
+// tags - метаинформация, массив ["premium", "promoted", "top"].
+
+                                                                                // ЗАДАЧА 2/41
+// Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
+
+// name - имя владельца, значение "Henry";
+// phone - телефон, значение "982-126-1588";
+// email - почта, значение "henry.carter@aptmail.com".
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  }
+};
