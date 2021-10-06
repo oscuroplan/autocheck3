066efc8aec15b6c40add42d9911c14bcfad59fa8
Maxim Levandovskiy <90237902+oscuroplan@users.noreply.github.com>
2021-10-06 11:13:25 +0300

    Update autocheck3.js

--- autocheck3.js
+++ autocheck3.js
@@ -7,6 +7,13 @@
 // price - цена, значение 2153;
 // tags - метаинформация, массив ["premium", "promoted", "top"].
 
+const apartment = {
+imgUrl: "https://via.placeholder.com/640x480",
+descr: "Spacious apartment in the city center",
+rating: 4,
+price: 2153,
+tags: ["premium", "promoted", "top"],
+};
                                                                                 // ЗАДАЧА 2/41
 // Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
 
@@ -27,3 +34,182 @@ const apartment = {
     email: "henry.carter@aptmail.com",
   }
 };
+
+
+                                                                                // ЗАДАЧА 3/41
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+// Change code below this line
+const aptRating = apartment.rating;
+const aptDescr = apartment.descr;
+const aptPrice = apartment.price;
+const aptTags = apartment.tags;
+// Change code above this line
+
+                                                                                // ЗАДАЧА 4/41
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// ownerName - имя владельца;
+// ownerPhone - телефон владельца;
+// ownerEmail - почта владельца;
+// numberOfTags - количество элементов массива в свойстве tags;
+// firstTag - первый элемент массива в свойстве tags;
+// lastTag - последний элемент массива в свойстве tags.
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[2];
+// Change code above this line
+
+                                                                                // ЗАДАЧА 5/41
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment используя синтаксис «квадратных скобок».
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+// Change code below this line
+const aptRating = apartment['rating'];
+const aptDescr = apartment['descr'];
+const aptPrice = apartment['price'];
+const aptTags = apartment['tags'];
+// Change code above this line
+
+                                                                                // ЗАДАЧА 6/41
+// Дополни код обновив значения свойств объекта apartment:
+
+// цену в свойстве price на 5000;
+// рейтинг квартиры в свойстве rating на 4.7;
+// имя владельца во вложенном свойстве name на "Henry Sibola";
+// массив тегов в свойстве tags добавив в конец строку "trusted".
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = 'Henry Sibola';
+apartment.tags.push('trusted');
+
+                                                                                // ЗАДАЧА 7/41
+// Добавь объекту apartment несколько новых свойств:
+
+// area - площадь в квадратных метрах, число 60;
+// rooms - количество комнат, число 3;
+// location - местоположение квартиры, обьект со следующими вложенными свойствами;
+// country - страна, строка "Jamaica";
+// city - город, строка "Kingston".
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = {
+  country: "Jamaica",
+  city: "Kingston",
+}
+
+                                                                                // ЗАДАЧА 8/41
+// Дополни код объявления объекта так, чтобы у него были свойства name,
+// price, image и tags со значениями из переменных с аналогичными именами.
+// Обязательно используй синтаксис коротких свойств.
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  // Change code below this line
+  name,
+  price,
+  image,
+  tags,
+  // Change code above this line
+};
+
+
+                                                                                // ЗАДАЧА 9/41
+// Дополни код объявления объекта credentials так, чтобы в результате у
+// него были два свойства: email и password, имена которых хранятся в 
+// переменных emailInputName и passwordInputName.
+
+// Значением свойства email должна быть строка
+// "henry.carter@aptmail.com", а значением свойства password - строка
+// "jqueryismyjam".
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  // Change code below this line
+[emailInputName]: "henry.carter@aptmail.com",
+[passwordInputName]: "jqueryismyjam",
+  // Change code above this line
+};
+
+
+                                                                                // ЗАДАЧА 10/41
+// Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств.
